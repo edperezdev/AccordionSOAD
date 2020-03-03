f703c1dc9a2eca394a0d99cf315be08790a7a868
Edwin <edch90@gmail.com>
2020-03-03 12:08:31 -0500

    Finalización de efectos con CSS y footer

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,14 @@ let acordeontitle = document.querySelectorAll(".acordeon-title");
 
 acordeontitle.forEach(title => {
   title.addEventListener("click", function() {
-    let acordeontitleactivo = document.querySelector(".acordeon-title.activo");
-    this.querySelector(".mas").classList.toggle("inactive");
-    this.querySelector(".menos").classList.toggle("active");
-    this.classList.toggle("activo");
+    let acordeontitleactivo = document.querySelector(".acordeon-title.active");
+
+    this.classList.toggle("active");
     let acordeonbody = this.nextElementSibling;
-    if (this.classList.contains("activo")) {
+    if (this.classList.contains("active")) {
       acordeonbody.style.maxHeight = acordeonbody.scrollHeight + "3px";
       if (acordeontitleactivo && acordeontitleactivo !== acordeontitle) {
-        acordeontitleactivo.classList.toggle("activo");
+        acordeontitleactivo.classList.toggle("active");
         acordeontitleactivo.nextElementSibling.style.maxHeight = 0;
       }
     } else {
